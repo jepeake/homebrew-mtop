5ac7427a6763593dc5b34310952298ed90779523
Jacob Peake <59978422+jepeake@users.noreply.github.com>
2024-12-21 17:48:23 +0000

    bump release

--- Formula/mtop.rb
+++ Formula/mtop.rb
@@ -1,8 +1,8 @@
 
 class Mtop < Formula
-  desc "a terminal-based performance monitor for apple silicon. implemented in rust."
+  desc "a terminal-based performance monitor for apple silicon"
   homepage "https://github.com/jepeake/mtop"
-  url "https://github.com/jepeake/mtop/releases/download/v0.2.0/mtop"
+  url "https://github.com/jepeake/mtop/releases/download/v0.3.0/mtop"
   sha256 "ce581297e425862a9203791185ac208c0bc772a4170b3251fa0e4fa98c8409a3"
   version "0.3.0"
   license "MIT" 
