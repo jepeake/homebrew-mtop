08fba27d463895c7f2cee8a08938f5de1e5047c1
Jacob Peake <59978422+jepeake@users.noreply.github.com>
2024-12-21 17:50:30 +0000

    bump sha256

--- Formula/mtop.rb
+++ Formula/mtop.rb
@@ -3,7 +3,7 @@ class Mtop < Formula
   desc "a terminal-based performance monitor for apple silicon"
   homepage "https://github.com/jepeake/mtop"
   url "https://github.com/jepeake/mtop/releases/download/v0.3.0/mtop"
-  sha256 "ce581297e425862a9203791185ac208c0bc772a4170b3251fa0e4fa98c8409a3"
+  sha256 "b845789fe0b47a2b7950b182d2cb426d2632f273a82f88a6ed316316c5621b36"
   version "0.3.0"
   license "MIT" 
 
